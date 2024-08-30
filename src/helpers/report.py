0bd3d7b0254dd775dc6eb534a58760aa6ba8ea79
Abdullah S. Naser <121259956+abdullahsn10@users.noreply.github.com>
2024-08-30 10:31:49 +0300

    New-Customers report endpoint (#8)
    * Add GET endpoint to list new customers
    
    * update docstrings

---
+++ src/helpers/report.py
@@ -0,0 +1,29 @@
+from datetime import date
+from sqlalchemy.orm import Session
+from src import schemas, models
+
+
+def list_new_customers(
+    db: Session, coffee_shop_id: int, from_date: date, to_date: date
+) -> schemas.NewCustomersReport:
+    """
+    This helper function lists all new customers along with their first order date
+    *Args:
+        db (Session): SQLAlchemy Session
+        coffee_shop_id (int): coffee shop id to filter customers
+        from_date (date): start date to filter orders
+        to_date (date): end date to filter orders
+    *Returns:
+        NewCustomersReport : number of new customers, list of new customers
+    """
+    query = db.query(models.Customer).filter(
+        models.Customer.coffee_shop_id == coffee_shop_id,
+        models.Customer.created >= from_date,
+        models.Customer.created <= to_date,
+    )
+    number_of_new_customers = query.count()
+
+    new_customers = query.all()
+    return schemas.NewCustomersReport(
+        number_of_new_customers=number_of_new_customers, new_customers=new_customers
+    )
--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -3,3 +3,4 @@ from src.schemas.branch import *
 from src.schemas.coffee_shop import *
 from src.schemas.customer import *
 from src.schemas.user import *
+from src.schemas.report import *
